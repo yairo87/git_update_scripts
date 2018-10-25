eff3f3cbac113a805c04d5cff9cf5e3a9aee1ad0
yairo87 <yairo@heed-global.com>
2018-10-25 10:46:44 +0300

    Update git_delete_merged_branches.sh

--- git_delete_merged_branches.sh
+++ git_delete_merged_branches.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git branch -r --merged | egrep -v "(^\*|master|develop)" | sed 's/origin\//:/' | xargs -n 1 git push origin
+git branch -r --merged | egrep -v "(^\*|master|develop|dev)" | sed 's/origin\//:/' | xargs -n 1 git push origin
